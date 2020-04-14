4321b56b42a1ca7e18e98e2d3e2860989ab4c6e9
Jovines <1246634075@qq.com>
2020-04-14 11:20:32 +0800

    修改sql脚本

--- sql/lbs.sql
+++ sql/lbs.sql
@@ -1,19 +1,16 @@
+drop database if exists lbs;
 create database lbs;
 show databases;
+
 use lbs;
 
 create table user
 (
     phone    bigint primary key,
     nickname varchar(30),
-    password char(16)
+    password char(30)
 );
 
-
-
 show tables;
-
-
-drop table user;
 
 select * from user;
